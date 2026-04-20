919dd23661b6c184a8b7c9a3617ee7dc41471216
Jan Reimann <jaanes79@gmail.com>
2026-04-20 16:11:23 +0300

    shrink mobile login card

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -111,8 +111,8 @@ function LoginPageContent() {
         <header className="header">
           <PlaceMeLogo
             className="logo"
-            markClassName="h-14 w-10 sm:h-[3.75rem] sm:w-[2.75rem]"
-            wordmarkClassName="!text-[2.9rem] sm:!text-[3.2rem]"
+            markClassName="h-11 w-8 sm:h-12 sm:w-9"
+            wordmarkClassName="!text-[2.3rem] sm:!text-[2.5rem]"
           />
         </header>
 
